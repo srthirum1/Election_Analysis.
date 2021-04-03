9d59a902ab7acc69d44ab2182dd4395a6efbf10a
Thirumalairajan <Sri.Thirumalairajan@thomsonreuters.com>
2021-04-03 19:50:38 -0400

    pushing new changes to pypoll

--- PyPoll.py
+++ PyPoll.py
@@ -4,41 +4,64 @@
 #Get the total votes for each candidate.
 #Get the total votes cast for the election.
 
-#import datetime as dt
-#now = dt.datetime.now()
-#print("this time:", now)
-
+# Add our dependencies.
 import csv
 import os
-#imports csv fule and dir is the directory
-#file_variable = open(filename, mode)
-# Assign a variable for the file to load and the path.
-#file_to_load = 'Resources/election_results.csv'
-#election_data = open(file_to_load, 'r')
-
-# Close the file.
-#election_data.close()
+# Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-
-# Create a filename variable to a direct or indirect path to the file.
+# Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Open the election results and read the file
-with open(file_to_load) as election_data:
+total_votes = 0
 
-     # Read the file object with the reader function.
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    # Print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
 
-# Using the open() function with the "w" mode we will write data to the file.
-# Using the with statement open the file as a text file.
-#with open(file_to_save, "w") as txt_file:
+    # Print each row in the CSV file.
+    for row in file_reader:
+        total_votes = total_votes + 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
 
-    # Write some data to the file.
-   
-    
-    #txt_file.write("Arapahoe\n, Denver,\n Jefferson")
 
+    for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+        print(f"{candidate_name}: received {vote_percentage}% of the vote.")
+        
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+     # 2. If true then set winning_count = votes and winning_percent =
+     # vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+     # 3. Set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+print(total_votes)
+print(candidate_options)
+print (candidate_votes)
